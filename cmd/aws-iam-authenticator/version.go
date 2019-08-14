9aca50a86c54c1a83b5f662e7fed358f7c994872
Christopher Hein <me@chrishein.com>
2019-08-13 16:24:21 -1000

    new vanity url for go-version
    Signed-off-by: Christopher Hein <me@chrishein.com>

--- cmd/aws-iam-authenticator/version.go
+++ cmd/aws-iam-authenticator/version.go
@@ -3,8 +3,8 @@ package main
 import (
 	"fmt"
 
-	goVersion "github.com/christopherhein/go-version"
 	"github.com/spf13/cobra"
+	goVersion "go.hein.dev/go-version"
 )
 
 var (
@@ -12,26 +12,21 @@ var (
 	version    = "unversioned"
 	commit     = ""
 	date       = ""
+	output     = ""
 	versionCmd = &cobra.Command{
 		Use:   "version",
 		Short: "Version will output the current build information",
 		Long:  ``,
 		Run: func(_ *cobra.Command, _ []string) {
-			var response string
-			versionOutput := goVersion.New(version, commit, date)
-
-			if shortened {
-				response = versionOutput.ToShortened()
-			} else {
-				response = versionOutput.ToJSON()
-			}
-			fmt.Printf("%+v", response)
+			resp := goVersion.FuncWithOutput(shortened, version, commit, date, output)
+			fmt.Print(resp)
 			return
 		},
 	}
 )
 
 func init() {
-	versionCmd.Flags().BoolVarP(&shortened, "short", "s", false, "Use shortened output for version information.")
+	versionCmd.Flags().BoolVarP(&shortened, "short", "s", false, "Print just the version number.")
+	versionCmd.Flags().StringVarP(&output, "output", "o", "json", "Output format. One of 'yaml' or 'json'.")
 	rootCmd.AddCommand(versionCmd)
 }
