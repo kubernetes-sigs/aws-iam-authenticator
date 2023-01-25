82981813b7e99a0f3d26e775497195ec2a714af3
Davanum Srinivas <davanum@gmail.com>
2023-01-25 15:46:14 -0500

    Add comments explicitly on what we need to do later
    Signed-off-by: Davanum Srinivas <davanum@gmail.com>

--- tests/integration/server/main_test.go
+++ tests/integration/server/main_test.go
@@ -38,6 +38,10 @@ func TestMain(m *testing.M) {
 	flag.StringVar(&roleARN, "role-arn", "", "ARN of role to be authenticated in the test. This role ARN is added to the configmap and it should be assumable by the test run.")
 	flag.StringVar(&testArtifactsDir, "test-artifacts-dir", "", "Directory used for artifacts generated from test runs.")
 
+	// TODO(dims): earlier we had `framework.EtcdMain` here. EtcdMain has code to check for number of goroutines leaked
+	// which trips us up. So using the hack to call `startEtcd` instead which is in the same module. The alternative was
+	// to use `RunCustomEtcd`, but that has a bug which is being fixed in upstream https://github.com/kubernetes/kubernetes/pull/115254
+	// once that gets available in a new release in kubernetes, we should switch to it.
 	flag.Parse()
 	stop, err := startEtcd()
 	if err != nil {
