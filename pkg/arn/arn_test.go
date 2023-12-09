e91d159bb339763102afc42169de058ed4b4f3c1
Daniel Kennedy <19178580+DanielCKennedy@users.noreply.github.com>
2023-12-08 19:10:38 -0800

    Fix role ARN comparison for user ID strict check

--- pkg/arn/arn_test.go
+++ pkg/arn/arn_test.go
@@ -33,3 +33,28 @@ func TestUserARN(t *testing.T) {
 		}
 	}
 }
+
+var arnStripTests = []struct {
+	arn      string // input arn
+	expected string // canonacalized arn
+	err      error  // expected error value
+}{
+	{"NOT AN ARN", "", fmt.Errorf("Not an arn")},
+	{"arn:aws:iam::123456789012:role/Org/Team/Admin", "arn:aws:iam::123456789012:role/Admin", nil},
+	{"arn:aws:iam::123456789012:role/Admin", "arn:aws:iam::123456789012:role/Admin", nil},
+	{"arn:aws:iam::123456789012:user/Alice", "arn:aws:iam::123456789012:user/Alice", nil},
+	{"arn:aws:sts::123456789012:federated-user/Bob", "arn:aws:sts::123456789012:federated-user/Bob", nil},
+}
+
+func TestStripPath(t *testing.T) {
+	for _, tc := range arnStripTests {
+		actual, err := StripPath(tc.arn)
+		if err != nil && tc.err == nil || err == nil && tc.err != nil {
+			t.Errorf("stripPath(%s) expected err: %v, actual err: %v", tc.arn, tc.err, err)
+			continue
+		}
+		if actual != tc.expected {
+			t.Errorf("stripPath(%s) expected: %s, actual: %s", tc.arn, tc.expected, actual)
+		}
+	}
+}
