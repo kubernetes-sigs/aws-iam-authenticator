adb560ed3c6ad769e1745d21c12e2c7c810bf594
Nicole Wren <wrenn@squareup.com>
2025-01-06 19:10:25 -0800

    Remove no-op err assignment

--- pkg/filecache/filecache.go
+++ pkg/filecache/filecache.go
@@ -252,7 +252,6 @@ func (f *FileCacheProvider) RetrieveWithContext(ctx context.Context) (credential
 		} else {
 			// credential doesn't support expiration time, so can't cache, but still return the credential
 			_, _ = fmt.Fprint(os.Stderr, "Unable to cache credential: credential doesn't support expiration\n")
-			err = nil
 		}
 		return V2CredentialToV1Value(credential), err
 	}
