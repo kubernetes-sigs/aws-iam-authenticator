4c0692830adcfae21ac304262804c540c59f732d
Bryant Biggs <bryantbiggs@gmail.com>
2025-09-08 19:24:16 -0500

    chore: Commit changes from `make codegen`

--- pkg/mapper/crd/generated/clientset/versioned/typed/iamauthenticator/v1alpha1/iamauthenticator_client.go
+++ pkg/mapper/crd/generated/clientset/versioned/typed/iamauthenticator/v1alpha1/iamauthenticator_client.go
@@ -44,9 +44,7 @@ func (c *IamauthenticatorV1alpha1Client) IAMIdentityMappings() IAMIdentityMappin
 // where httpClient was generated with rest.HTTPClientFor(c).
 func NewForConfig(c *rest.Config) (*IamauthenticatorV1alpha1Client, error) {
 	config := *c
-	if err := setConfigDefaults(&config); err != nil {
-		return nil, err
-	}
+	setConfigDefaults(&config)
 	httpClient, err := rest.HTTPClientFor(&config)
 	if err != nil {
 		return nil, err
@@ -58,9 +56,7 @@ func NewForConfig(c *rest.Config) (*IamauthenticatorV1alpha1Client, error) {
 // Note the http client provided takes precedence over the configured transport values.
 func NewForConfigAndClient(c *rest.Config, h *http.Client) (*IamauthenticatorV1alpha1Client, error) {
 	config := *c
-	if err := setConfigDefaults(&config); err != nil {
-		return nil, err
-	}
+	setConfigDefaults(&config)
 	client, err := rest.RESTClientForConfigAndClient(&config, h)
 	if err != nil {
 		return nil, err
@@ -83,7 +79,7 @@ func New(c rest.Interface) *IamauthenticatorV1alpha1Client {
 	return &IamauthenticatorV1alpha1Client{c}
 }
 
-func setConfigDefaults(config *rest.Config) error {
+func setConfigDefaults(config *rest.Config) {
 	gv := iamauthenticatorv1alpha1.SchemeGroupVersion
 	config.GroupVersion = &gv
 	config.APIPath = "/apis"
@@ -92,8 +88,6 @@ func setConfigDefaults(config *rest.Config) error {
 	if config.UserAgent == "" {
 		config.UserAgent = rest.DefaultKubernetesUserAgent()
 	}
-
-	return nil
 }
 
 // RESTClient returns a RESTClient that is used to communicate
